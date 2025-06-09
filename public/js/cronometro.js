9765e213e083b6ecf5ae997e68c23a14e6714fcd
Caio Rodrigo <contato@caiorodrigocev.com.br>
2025-06-08 21:51:01 -0300

    Replace session expiry text with animated clock icon

--- public/js/cronometro.js
+++ public/js/cronometro.js
@@ -23,7 +23,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   
-  document.getElementById("cronometro").innerHTML = "Sessão expira em: " + minutes + "m " + seconds + "s ";
+  document.getElementById("cronometro").innerHTML =
+    "<i class='fas fa-clock fa-spin'></i> " + minutes + "m " + seconds + "s";
     
   // If the count down is over, write some text 
   if (distance < 0) {
